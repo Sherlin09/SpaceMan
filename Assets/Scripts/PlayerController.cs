b5cc9c6e93b7fc120ccf1f33e77d9514e37af969
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-09-24 12:36:01 -0500

    1_12 Cambiar las animaciones

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,24 @@ public class PlayerController : MonoBehaviour
     // Variables del movimiento del personaje
     public float jumpForce = 6f;
     Rigidbody2D rigidBody;
+    Animator animator;
+
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
 
     public LayerMask groundMask;
 
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
     }
 
     // Update is called once per frame
@@ -28,6 +34,8 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Jump();
         }
+
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
 
 
         Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
@@ -44,9 +52,11 @@ public class PlayerController : MonoBehaviour
     bool IsTouchingTheGround(){
         if(Physics2D.Raycast(this.transform.position, Vector2.down, 1.5f, groundMask)){
             //TODO: Programar logica de contacto con el suleo
+            animator.enabled = true;
             return true;
         } else {
             //TODO: programar logica d econtacto
+            animator.enabled = false;
             return false;
         }
     }
